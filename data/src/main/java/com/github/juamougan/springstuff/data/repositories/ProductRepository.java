dd1f19c385745cc974e2e1b5f42cb9cc15bd62dc
Juan Manuel Mougan <juanmougan@gmail.com>
2019-07-03 16:34:04 -0300

    Removed unnecessary method

--- data/src/main/java/com/github/juamougan/springstuff/data/repositories/ProductRepository.java
+++ data/src/main/java/com/github/juamougan/springstuff/data/repositories/ProductRepository.java
@@ -7,8 +7,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface ProductRepository extends CrudRepository<Product, Long> {    // So far, CrudRepository is enough
 
-  @Override <S extends Product> S save(S entity);
-
   Product findByName(String name);
 
 }
